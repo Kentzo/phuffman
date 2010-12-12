577b9c4e6a9557d541890affdba653a124d3aad2
linux <dlyfar@dlyfar-desktop.(none)>
2010-12-12 17:40:58 +0600

    gpu based length encoding

--- demo/main.cpp
+++ demo/main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 using namespace phuffman;
 
-//extern "C" Row* runEncode(unsigned char* a_data, size_t len, Row a_table[256]);
+extern "C" void runEncode(unsigned char* a_data, size_t len, CodesTable a_table, unsigned char* result, size_t* result_len);
 
 void PrintCodesTable(const CodesTableAdapter& table) {
     for (size_t i=0; i<ALPHABET_SIZE; ++i) {
@@ -17,83 +17,36 @@ void PrintCodesTable(const CodesTableAdapter& table) {
 }
 
 int main() {
-    unsigned char test[ALPHABET_SIZE] = {0};
-    for (size_t i=0; i<ALPHABET_SIZE; ++i) {
+const  size_t dlen = ALPHABET_SIZE;
+    unsigned char test[dlen] = {0};
+    for (size_t i=0; i<dlen; ++i) {
     	test[i] = i;
     }
-    cout << test << endl;
-    cout << "Build codes table from string" << endl;
+
     CodesTableAdapter codes(test, sizeof(test)/sizeof(unsigned char)-1);
-    PrintCodesTable(codes);
+    unsigned char* result = new unsigned char[codes.c_table()->info.maximum_codelength * dlen];
+    size_t resLen = 0;
+    runEncode(test, dlen, *codes.c_table(), result, &resLen);
+
+    cout << "Table CPU:" << endl;
+    for (size_t i = 0; i < dlen; ++i) {
+      cout << (int)test[i] << " " << (int)codes[test[i]].codelength << endl;
+    }
+    
+    delete [] result;
+
+    //cout << test << endl;
+    //cout << "Build codes table from string" << endl;
+    
+    //    PrintCodesTable(codes);
     stringstream ss;
     ss << codes;
     ss.seekg(0, ios_base::beg);
 
     char* file_data = new char[ALPHABET_SIZE];
     ss.read(file_data, ALPHABET_SIZE);
-    cout << "Build codes table from file data" << endl;
+    //cout << "Build codes table from file data" << endl;
     CodesTableAdapter cc(file_data, ALPHABET_SIZE);
-    PrintCodesTable(cc);
-    cout << "Codes Tables are equal: " << (codes == cc) << endl;
-    /*
-  ifstream file("../test");
-  file.seekg (0, ios::end);
-  unsigned int len = file.tellg();
-  file.seekg (0, ios::beg);
-  char* buffer = new char [len];
-  file.read(buffer, len);
-
-  HuffmanTable h((const unsigned char*)buffer, len);
-
-  Row rows[256];
-  for (size_t i = 0; i < HuffmanTable::ALPHABET_SIZE; ++i) {
-    rows[i] = h[i];
-  }
-
-  Row* cpu_encoded = new Row[len];
-
-  // work on GPU
-  Row* encoded = runEncode((unsigned char*)buffer, len, rows);
-
-  //work on CPU
-  for (size_t i = 0; i < len; ++i) {
-    cpu_encoded[i] = rows[(unsigned char)buffer[i]];      
-  }
-
-  // comparison
-  for (size_t i = 0; i < len; ++i) {
-    if (encoded[i].code != cpu_encoded[i].code || encoded[i].codelength != cpu_encoded[i].codelength) {
-      cout << "Comparison result: " << i << " " << (int)encoded[i].code << "\t" << (int)cpu_encoded[i].code << endl;
-      break;
-    }
-  }
-
-  // CPU decoding
-  char *decoded = new char[len];
-  for (unsigned int i=0; i<len; ++i) {
-      Row curRow = cpu_encoded[i];
-      unsigned char c = 0;
-      for (unsigned char j=0; j<HuffmanTable::ALPHABET_SIZE; ++j) {
-          if (rows[j].code == curRow.code && rows[j].codelength == curRow.codelength) {
-              c = j;
-              break;
-          }
-      }
-      decoded[i] = c;
-  }
-
-  // comparison with initial string
-  for (size_t i = 0; i < len; ++i) {
-    if (decoded[i] != buffer[i]) {
-      cout << "Decoding comparison result: " << i << " " << (int)decoded[i] << "\t" << (int)buffer[i] << endl;
-      break;
-    }
-  }
-  cout << "Compare decoded and initial strings: " << memcmp(decoded, buffer, len) << endl;
-
-  delete[] decoded;
-  delete[] cpu_encoded;
-  free(encoded);
-  delete[] buffer;*/
-  return 0; 
+    //PrintCodesTable(cc);
+    //cout << "Codes Tables are equal: " << (codes == cc) << endl;
 }
--- src/CUDA/encode.cu
+++ src/CUDA/encode.cu
@@ -1,47 +1,86 @@
 #include <cuda.h>
 #include <cuda_runtime_api.h>
 #include <cutil_inline.h>
-#include "Row.h"
+#include "Code.h"
+#include "constants.h"
+#include "CodesTable.h"
 #include <stdio.h>
+#include <math.h>
+#include <stddef.h>
 
-__constant__ Row table[256];
+__constant__ CodesTable table;
 
-__global__ void encode(unsigned char* a_data, Row* a_result, size_t len, size_t num)
+__global__ void length_encode(unsigned char* a_data, unsigned int* a_result, size_t num, size_t totalSize)
 {  
   unsigned int tx = threadIdx.x;
   unsigned int bx = blockIdx.x;
   unsigned int idx = blockDim.x*bx + tx;
-
+  if (totalSize < idx)
+    return;
+  num = min((unsigned long long)num, (unsigned long long)totalSize - idx);
   for (int i = 0; i < num; ++i) {
      int cidx = idx + i;
      unsigned char c = a_data[cidx];
-     Row row = table[c];     
-     a_result[cidx] = row;
+     Code row = table.codes[c];     
+     a_result[cidx] = (unsigned int)row.codelength;
   }
 }
 
 extern "C" {
-  Row* runEncode(unsigned char* a_data, size_t len, Row a_table[256])
+  void runEncode(unsigned char* a_data, size_t len, CodesTable a_table, unsigned char* result, size_t* result_len)
   {
-    cutilSafeCall(cudaMemcpyToSymbol(table, a_table, sizeof(Row) * 256));
+    int deviceCount = 0;
+    if (cudaGetDeviceCount(&deviceCount) != cudaSuccess) {
+      return;
+    }
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, deviceCount - 1);
+    float totalGlobal = deviceProp.totalGlobalMem;    
+    float maxCodeLen = ceilf((float)a_table.info.maximum_codelength / 8);
+    float maxDataLen = floorf((totalGlobal - sizeof(table))/(5 + maxCodeLen)) ;
 
+    static const size_t num = 32;
+    static const size_t bsize = 512;
+
+    cutilSafeCall(cudaMemcpyToSymbol(table, &a_table, sizeof(CodesTable)));
     void* devData = NULL;
-    void* result = NULL;
-    cutilSafeCall(cudaMalloc(&devData, len));
-    cutilSafeCall(cudaMemcpy(devData, a_data, len, cudaMemcpyHostToDevice));
-  
-    cutilSafeCall(cudaMalloc(&result, len * sizeof(Row)));
+    void* devResultAddr = NULL;
+    void* devResult = NULL;
+    while (len != 0) {
+      long long dataLen = min((long long)len, (long long)maxDataLen); // count of elements
+      int bytesCopy = 0;
 
-    dim3 grid(44050, 1, 1);
-    dim3 block(512, 1, 1);
-    encode<<<grid, block>>>((unsigned char*)devData, (Row*)result, len, 1);
-		  
-    Row* res = (Row*)calloc(len, sizeof(Row));
-    cutilSafeCall(cudaMemcpy(res, result, len * sizeof(Row), cudaMemcpyDeviceToHost));
+      cutilSafeCall(cudaMalloc(&devData, dataLen));
+      cutilSafeCall(cudaMalloc(&devResultAddr, dataLen*sizeof(unsigned int)));
+      cutilSafeCall(cudaMalloc(&devResult, dataLen*maxCodeLen));
+      cutilSafeCall(cudaMemcpy(devData, a_data, dataLen, cudaMemcpyHostToDevice));
 
-    cudaFree(devData);
-    cudaFree(result);  
-    return res;
+      float gsize = (float(dataLen) / (num * bsize)); 
+
+      gsize = ceilf(gsize);
+      printf("gsize = %f\n", gsize);
+      
+      dim3 grid(gsize, 1, 1);
+      dim3 block(bsize, 1, 1);
+      length_encode<<<grid, block>>>((unsigned char*)devData, (unsigned int*)devResultAddr, num, dataLen);
+      
+
+      cutilSafeCall(cudaMemcpy(&bytesCopy, (unsigned int*)devResultAddr + dataLen - 1, sizeof(unsigned int), cudaMemcpyDeviceToHost));
+      unsigned int* devResultAddrHost = (unsigned int*)calloc(dataLen, sizeof(unsigned int));
+      cutilSafeCall(cudaMemcpy(devResultAddrHost, devResultAddr, dataLen*sizeof(unsigned int), cudaMemcpyDeviceToHost));
+      for (int i = 0; i < dataLen; ++i) {
+	printf("%i %i\n ", i, devResultAddrHost[i]);
+      }
+      free(devResultAddrHost);
+      
+      cutilSafeCall(cudaMemcpy(result, devResult, bytesCopy, cudaMemcpyDeviceToHost));
+
+      cudaFree(devData);
+      cudaFree(devResultAddr);
+      cudaFree(devResult);
+
+      len -= dataLen;
+    }    
   }
 }
 
