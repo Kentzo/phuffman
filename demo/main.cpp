0ee228b20b590ec2f47c7e3222661368ba83a446
Ilya Kulakov <kulakov.ilya@gmail.com>
2010-12-13 12:47:06 +0600

    CHG: test OpenCL implementation of EncodeWithCodelengths function

--- demo/main.cpp
+++ demo/main.cpp
@@ -16,7 +16,7 @@ void PrintCodesTable(const CodeTableAdapter& table) {
     }
 }
 
-int main() {
+void TestCodeTableBuilder() {
     unsigned char test[ALPHABET_SIZE] = {0};
     for (size_t i=0; i<ALPHABET_SIZE; ++i) {
     	test[i] = i;
@@ -35,65 +35,23 @@ int main() {
     CodeTableAdapter cc(file_data, ALPHABET_SIZE);
     PrintCodesTable(cc);
     cout << "Codes Tables are equal: " << (codes == cc) << endl;
-    /*
-  ifstream file("../test");
-  file.seekg (0, ios::end);
-  unsigned int len = file.tellg();
-  file.seekg (0, ios::beg);
-  char* buffer = new char [len];
-  file.read(buffer, len);
+}
 
-  HuffmanTable h((const unsigned char*)buffer, len);
-
-  Row rows[256];
-  for (size_t i = 0; i < HuffmanTable::ALPHABET_SIZE; ++i) {
-    rows[i] = h[i];
-  }
-
-  Row* cpu_encoded = new Row[len];
-
-  // work on GPU
-  Row* encoded = runEncode((unsigned char*)buffer, len, rows);
-
-  //work on CPU
-  for (size_t i = 0; i < len; ++i) {
-    cpu_encoded[i] = rows[(unsigned char)buffer[i]];      
-  }
-
-  // comparison
-  for (size_t i = 0; i < len; ++i) {
-    if (encoded[i].code != cpu_encoded[i].code || encoded[i].codelength != cpu_encoded[i].codelength) {
-      cout << "Comparison result: " << i << " " << (int)encoded[i].code << "\t" << (int)cpu_encoded[i].code << endl;
-      break;
+int main() {
+    static const size_t test_size = ALPHABET_SIZE;
+    unsigned char* test = new unsigned char[test_size];
+    for (size_t i=0; i<test_size; ++i) {
+        test[i] = i;
     }
-  }
-
-  // CPU decoding
-  char *decoded = new char[len];
-  for (unsigned int i=0; i<len; ++i) {
-      Row curRow = cpu_encoded[i];
-      unsigned char c = 0;
-      for (unsigned char j=0; j<HuffmanTable::ALPHABET_SIZE; ++j) {
-          if (rows[j].code == curRow.code && rows[j].codelength == curRow.codelength) {
-              c = j;
-              break;
-          }
-      }
-      decoded[i] = c;
-  }
-
-  // comparison with initial string
-  for (size_t i = 0; i < len; ++i) {
-    if (decoded[i] != buffer[i]) {
-      cout << "Decoding comparison result: " << i << " " << (int)decoded[i] << "\t" << (int)buffer[i] << endl;
-      break;
+    try {
+        unsigned char* result = NULL;
+        size_t result_size = 0;
+        opencl::Encode(test, test_size, &result, &result_size);
+        delete[] result;
     }
-  }
-  cout << "Compare decoded and initial strings: " << memcmp(decoded, buffer, len) << endl;
-
-  delete[] decoded;
-  delete[] cpu_encoded;
-  free(encoded);
-  delete[] buffer;*/
-  return 0; 
+    catch(cl::Error err) {
+        cerr << err.err() << endl;
+    }
+    delete[] test;
+    return 0; 
 }
