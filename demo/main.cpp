ab4d08ccfa538b125e895ad0363a640062538ed8
linux <dlyfar@dlyfar-desktop.(none)>
2011-02-15 23:17:33 +0600

    huffman encoding finally implemented

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,12 +1,13 @@
 #include "phuffman.hpp"
 #include <iostream>
 #include <sstream>
+#include <string.h>
 
 
 using namespace std;
 using namespace phuffman;
 
-extern "C" void runEncode(unsigned char* a_data, size_t len, CodesTable a_table, unsigned char* result, size_t* result_len);
+extern "C" void runEncode(unsigned char* a_data, size_t len, CodesTable a_table, unsigned int* result, size_t* result_len);
 
 void PrintCodesTable(const CodesTableAdapter& table) {
     for (size_t i=0; i<ALPHABET_SIZE; ++i) {
@@ -17,37 +18,43 @@ void PrintCodesTable(const CodesTableAdapter& table) {
 }
 
 int main() {
-  const  size_t dlen = ALPHABET_SIZE*1024*1;
-  unsigned char* test = new unsigned char[dlen];//{0};
-  for (size_t i=0; i<dlen; ++i) {
-    	test[i] = i;
-    }
+  
+  unsigned char test[] = {'a', 'a', 'a', 'c', 'c', 'c', 'a', 'c', 'a', 'b', 'b', 'b', 'b', 'a', 'a', 'a', 'c', 'b', 'b', 'b', 'c', 'a', 'a', 'c', 'b', 'b', 'c', 'a', 'a', 'a', 'b', 'b', 'b', 'c', 'a', 'a', 'c', 'b', 'b', 'b', 'b', 'a', 'c', 'a', 'b', 'b', 'b', 'b', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'a', 'a', 'c', 'b', 'b', 'b'};//(unsigned char*)"aaabbb";//new unsigned char[dlen];//{0};
+  const  size_t dlen = sizeof(test)/sizeof(char);//ALPHABET_SIZE*1024*1;
 
     CodesTableAdapter codes(test, dlen);
-    unsigned char* result = new unsigned char[codes.c_table()->info.maximum_codelength * dlen];
-    size_t resLen = 0;
+
+    unsigned int result[100] = {0};
+    size_t resLen = 16;
     runEncode(test, dlen, *codes.c_table(), result, &resLen);
 
-    cout << "Table CPU:" << endl;
-    for (size_t i = 0; i < dlen; ++i) {
+    cout << "Result: ";
+    for (size_t i = 0; i < resLen; ++i) {
+        cout << result[i] << " ";
       //            cout << (int)test[i] << " " << (int)codes[test[i]].codelength << endl;
     }
+    cout << endl;
+
+//    cout << "manual: ";
+//    for (int i=0; i<dlen; ++i) {
+//        cout << (int)codes[test[i]].code << "(" << (int)codes[test[i]].codelength << ")" << " ";
+//    }
+//    cout << endl;
     
-    delete [] result;
-    delete [] test;
+//    delete [] test;
 
     //cout << test << endl;
     //cout << "Build codes table from string" << endl;
     
-    //    PrintCodesTable(codes);
-    stringstream ss;
-    ss << codes;
-    ss.seekg(0, ios_base::beg);
+    PrintCodesTable(codes);
+   // stringstream ss;
+    //ss << codes;
+   // ss.seekg(0, ios_base::beg);
 
-    char* file_data = new char[ALPHABET_SIZE];
-    ss.read(file_data, ALPHABET_SIZE);
+   // char* file_data = new char[ALPHABET_SIZE];
+  //  ss.read(file_data, ALPHABET_SIZE);
     //cout << "Build codes table from file data" << endl;
-    CodesTableAdapter cc(file_data, ALPHABET_SIZE);
+   // CodesTableAdapter cc(file_data, ALPHABET_SIZE);
     //PrintCodesTable(cc);
     //cout << "Codes Tables are equal: " << (codes == cc) << endl;
 }
--- src/CUDA/encode.cu
+++ src/CUDA/encode.cu
@@ -15,8 +15,11 @@ __constant__ Code codes[ALPHABET_SIZE];
 
 __device__ int4 left_shift_int4(int4 a_value, unsigned int a_num)
 {
+//    cuPrintf("left_shift_int4([%i, %i, %i, %i], %i)\n", a_value.x, a_value.y, a_value.z, a_value.w, a_num);
+//    cuPrintf("\t%i\n", 1 << 28);
     int w_l = a_value.w << (a_num % 32);
     int w_h = a_value.w >> (32 - (a_num % 32));
+
 
     int z_l = a_value.z << (a_num % 32);
     int z_h = a_value.z >> (32 - (a_num % 32));
@@ -26,13 +29,21 @@ __device__ int4 left_shift_int4(int4 a_value, unsigned int a_num)
 
     int x_l = a_value.x << (a_num % 32);
 
-    if (a_num <= 32) {
+//    cuPrintf("\tx_l: %i\n", x_l);
+//    cuPrintf("\ty_l: %i\n", y_l);
+//    cuPrintf("\ty_h: %i\n", y_h);
+//    cuPrintf("\tz_l: %i\n", z_l);
+//    cuPrintf("\tz_h: %i\n", z_h);
+//    cuPrintf("\tw_l: %i\n", w_l);
+//    cuPrintf("\tw_h: %i\n", w_h);
+
+    if (a_num < 32) {
         a_value = make_int4(x_l | y_h, y_l | z_h, z_l | w_h, w_l);
     }
-    else if (a_num > 32 && a_num <= 64) {
+    else if (a_num >= 32 && a_num < 64) {
         a_value = make_int4(y_l | z_h, z_l | w_h, w_l, 0);
     }
-    else if (a_num > 64 && a_num <= 96) {
+    else if (a_num >= 64 && a_num < 96) {
         a_value = make_int4(z_l | w_h, w_l, 0, 0);
     }
     else {
@@ -55,13 +66,13 @@ __device__ int4 right_shift_int4(int4 a_value, unsigned int a_num)
     int x_h = a_value.x >> (a_num % 32);
     int x_l = a_value.x << (32 - (a_num %32));
 
-    if (a_num <= 32) {
+    if (a_num < 32) {
         a_value = make_int4(x_h, y_h | x_l, z_h | y_l, w_h | z_l);
     }
-    else if (a_num > 32 && a_num <= 64) {
+    else if (a_num >= 32 && a_num < 64) {
         a_value = make_int4(0, x_h, y_h | x_l, z_h | y_l);
     }
-    else if (a_num > 64 && a_num <= 96) {
+    else if (a_num >= 64 && a_num < 96) {
         a_value = make_int4(0, 0, x_h, y_h | x_l);
     }
     else {
@@ -77,13 +88,52 @@ __device__ int4 or_int4(int4 a_value1, int4 a_value2)
 }
 
 __device__ void merge_int4(int4 a_value1, int a_value1_zeroes, int4 a_value2, int a_value2_zeroes, int4 (*result)[2]) {
-    int4 a_value2_h = right_shift_int4(a_value2, sizeof(int4) - a_value1_zeroes);
+    int4 a_value2_h = right_shift_int4(a_value2, sizeof(int4) * 8 - a_value1_zeroes);
     int4 a_value2_l = left_shift_int4(a_value2, a_value1_zeroes);
     *result[0] = or_int4(a_value1, a_value2_h);
     *result[1] = a_value2_l;
 }
 
 
+// TODO: проверка на выход за границы массива результата
+__device__ void merge_atomic(int4 a_value1[2], unsigned int *result) {
+    cuPrintf("merge_atomic\n");
+    cuPrintf("%p\n", result);
+    cuPrintf("\ta_avlue[0]: %u %u %u %u\n", a_value1[0].x, a_value1[0].y, a_value1[0].z, a_value1[0].w);
+    cuPrintf("\ta_value[1]: %u %u %u %u\n", a_value1[1].x, a_value1[1].y, a_value1[1].z, a_value1[1].w);
+
+    cuPrintf("\tbefore: result[0]: %u\n", *result);
+    atomicOr(result, a_value1[0].x);
+    cuPrintf("\tafter: result[0]: %u\n", *result);
+
+    cuPrintf("\tbefore:result[1]: %u\n", *(result + 1));
+    atomicOr(result + 1, a_value1[0].y);
+    cuPrintf("\tafter: result[1]: %u\n", *(result + 1));
+
+    cuPrintf("\tbefore:result[2]: %u\n", *(result + 2));
+    atomicOr(result + 2, a_value1[0].z);
+    cuPrintf("\tafter: result[2]: %u\n", *(result + 2));
+
+    cuPrintf("\tbefore:result[3]: %u\n", *(result + 3));
+    atomicOr(result + 3, a_value1[0].w);
+    cuPrintf("\tafter: result[3]: %u\n", *(result + 3));
+
+    cuPrintf("\tbefore:result[4]: %u\n", *(result + 4));
+    atomicOr(result + 4, a_value1[1].x);
+    cuPrintf("\tafter: result[4]: %u\n", *(result + 4));
+
+    cuPrintf("\tbefore:result[5]: %u\n", *(result + 5));
+    atomicOr(result + 5, a_value1[1].y);
+    cuPrintf("\tafter: result[5]: %u\n", *(result + 5));
+
+    cuPrintf("\tbefore:result[6]: %u\n", *(result + 6));
+    atomicOr(result + 6, a_value1[1].z);
+    cuPrintf("\tafter: result[6]: %u\n", *(result + 6));
+
+    cuPrintf("\tbefore:result[7]: %u\n", *(result + 7));
+    atomicOr(result + 7, a_value1[1].w);
+    cuPrintf("\tafter: result[7]: %u\n", *(result + 7));
+}
 
 __global__ void length_encode(unsigned char* a_data, unsigned int a_data_len, unsigned int* a_result, unsigned int num_to_process)
 {
@@ -98,45 +148,70 @@ __global__ void length_encode(unsigned char* a_data, unsigned int a_data_len, un
   }
 }
 
-__global__ void encode(unsigned char* a_data, unsigned int a_data_len, unsigned char* a_prefix, unsigned int num_to_process)
+__global__ void encode(unsigned char* a_data, unsigned int a_data_len, unsigned int* a_prefix, unsigned int num_to_process,
+                        unsigned int* a_result)
 {
     unsigned int bx = threadIdx.x;
-    const int NUM_OF_SYM = 5;
+    const int NUM_OF_SYM = 4;
+
+    //cuPrintf("%s\n", "encode");
     for (int block_idx = 0; block_idx < num_to_process; block_idx += NUM_OF_SYM) {
         unsigned int b_idx = min(bx * num_to_process + block_idx, a_data_len);
         unsigned int e_idx = min(b_idx + NUM_OF_SYM, a_data_len);
 
+        if (b_idx == a_data_len)
+            return;
+        
+        cuPrintf("block: %i\n", block_idx);
+
         // Encode
-        int4 buf[2];
-        int buf_idx = 0; // buffer index
-        int x = 0;
-        int y = 0;
+        int4 buf[2] = {make_int4(0, 0, 0, 0), make_int4(0, 0, 0, 0)}; // Буфер должен вмещать NUM_OF_SYM кодов по MAXIMUM_CODELENGTH битов
+        int buf_idx = 0;
+        int towrite = 0;
+        // Пропускаем result_start_bit % sizeof(int) битов, т.к. они будет использованы другим потоком
+        unsigned int result_start_bit = a_prefix[b_idx];
+        int written = result_start_bit % (sizeof(int) * 8);
+        cuPrintf("written: %i\n", written);
         for (int j = b_idx; j < e_idx; ++j) {
+            cuPrintf("\tj: %i\n", j);
             unsigned char c = a_data[j];
-            x = sizeof(int4) - codes[c].codelength - y;
-            if (x >= 0) {
-                x = 0;
+            towrite = sizeof(int4) * 8 - codes[c].codelength - written;
+            // Влазим в текущий буфер
+            if (towrite >= 0) {
+                //00001100000000000000000000000000
+                //00010000000000000000000000000000
+                // Сдвигаем код так, чтобы в результате OR он добавился к остальным без пропусков
+                //int4 tmp = make_int4(0, 0, 0, codes[c].code);
+                //int4 tmp_shift = left_shift_int4(tmp, towrite);
+                //cuPrintf("tmp: [%i, %i, %i, %i], towrite: %i, tmp_shift: [%i, %i, %i, %i]\n", tmp.x, tmp.y, tmp.z, tmp.w, towrite, tmp_shift.x, tmp_shift.y, tmp_shift.z, tmp_shift.w);
                 buf[buf_idx] = or_int4(buf[buf_idx],
-                        left_shift_int4(make_int4(0, 0, 0, codes[c].code), sizeof(int4) - codes[c].codelength - y));
-                y += codes[c].codelength;
+                        left_shift_int4(make_int4(0, 0, 0, codes[c].code), towrite));
+                written += codes[c].codelength;
+                towrite = 0;
             }
+            // Переносим towrite кодов в следущий буфер
             else {
-                x = -x;
-                int4 code = left_shift_int4(make_int4(0, 0, 0, codes[c].code), sizeof(int4) - codes[c].codelength);
-                merge_int4(buf[0], sizeof(int4) - y, code, sizeof(int4) - codes[c].codelength, &buf);
-                /*int2 high_bits = make_int2(0, codes[c].code) >> x;
-                int2 low_bits = make_int2(0, code[c].code) << sizeof(int2) - x;
-                buf[buf_idx] |= high_bits;
-                buf[buf_idx+1] |= low_bits;*/
+                cuPrintf("FIND_ME\n");
+                towrite = -towrite;
+                // Разделяем код на 2 части: одну для текущего буфера, другую для следующего
+                // Нужно сохранить старшие нули
+                int4 code = left_shift_int4(make_int4(0, 0, 0, codes[c].code), sizeof(int4) * 8 - codes[c].codelength);
+                merge_int4(buf[0], sizeof(int4) * 8 - written, code, (sizeof(int4) * 8) - codes[c].codelength, &buf);
                 ++buf_idx;
             }
         }
+        cuPrintf("buf[0]: %i %i %i %i\n", buf[0].x, buf[0].y, buf[0].z, buf[0].w);
+        //cuPrintf("buf[1]: %i %i %i %i\n", buf[1].x, buf[1].y, buf[1].z, buf[1].w);
+        //cuPrintf("[%i %i %i %i] [%i %i %i %i]\n", buf[0].x, buf[0].y, buf[0].z, buf[0].w,
+        //        buf[1].x, buf[1].y, buf[1].z, buf[1].w);
+        unsigned int result_start_int = (result_start_bit - (result_start_bit % (sizeof(int) * 8))) / (sizeof(int) * 8);
+        merge_atomic(buf, a_result + result_start_int);
     }
 }
 
 extern "C" {
 
-  void runEncode(unsigned char* a_data, size_t len, CodesTable a_table, unsigned char* result, size_t* result_len)
+  void runEncode(unsigned char* a_data, size_t len, CodesTable a_table, unsigned int* result, size_t* result_len)
   {
     cudaPrintfInit();
     int deviceCount = 0;
@@ -146,7 +221,7 @@ extern "C" {
 
     cudaDeviceProp deviceProp;
     cudaGetDeviceProperties(&deviceProp, deviceCount - 1);
-    float totalGlobal = deviceProp.totalGlobalMem;    
+    float totalGlobal = deviceProp.totalGlobalMem;
     float maxCodeLen = ceilf((float)a_table.info.maximum_codelength / 8);
     float maxDataLen = floorf((totalGlobal - sizeof(codes))/(5 + maxCodeLen)) ;
     printf("max codelength: %f\n", maxCodeLen);
@@ -163,30 +238,44 @@ extern "C" {
       long long dataLen = min((long long)len, (long long)maxDataLen); // count of elements
       int bytesCopy = 0;
 
+      int res_size = dataLen*maxCodeLen + sizeof(int) - (dataLen*(int)maxCodeLen % sizeof(int));
       cutilSafeCall(cudaMalloc(&devData, dataLen));
       cutilSafeCall(cudaMalloc(&devResultAddr, dataLen*sizeof(unsigned int)));
-      cutilSafeCall(cudaMalloc(&devResult, dataLen*maxCodeLen));
+      cutilSafeCall(cudaMalloc(&devResult, res_size));
+      cutilSafeCall(cudaMemset(devResult, 0, res_size));
+
+      printf("start: %p, end: %p\n", (unsigned char *)devResult, (unsigned char *)devResult + res_size);
+
+//      unsigned int* test = (unsigned int*)calloc(res_size, 1);
+//      cutilSafeCall(cudaMemcpy(test, devResult, res_size, cudaMemcpyDeviceToHost));
+//      for (int i = 0; i < res_size/sizeof(int); ++i) {
+//          printf("%i\n", test[i]);
+//      }
+//      free(test);
+
       cutilSafeCall(cudaMemcpy(devData, a_data, dataLen, cudaMemcpyHostToDevice));
 
       length_encode<<<grid_range, block_range>>>((unsigned char*)devData, dataLen, (unsigned int*)devResultAddr, num);
       
       thrust::device_ptr<unsigned int> devPtr((unsigned int*)devResultAddr);
-      thrust::inclusive_scan(devPtr, devPtr + dataLen, devPtr);
+      thrust::exclusive_scan(devPtr, devPtr + dataLen, devPtr);
 
-
+      encode<<<grid_range, block_range>>>((unsigned char*)devData, dataLen, (unsigned int*)devResultAddr, num, (unsigned int*)devResult);
 
       cudaPrintfDisplay(stdout, true); 
       cudaPrintfEnd();
+      
+      *result_len = res_size / sizeof(int) ;
+      cutilSafeCall(cudaMemcpy(result, devResult, res_size, cudaMemcpyDeviceToHost));
+      //cuPrintf("[%i %i %i] %i\n", );
 
-      cutilSafeCall(cudaMemcpy(&bytesCopy, (unsigned int*)devResultAddr + dataLen - 1, sizeof(unsigned int), cudaMemcpyDeviceToHost));
+      printf("Prefix sum:\n");
       unsigned int* devResultAddrHost = (unsigned int*)calloc(dataLen, sizeof(unsigned int));
       cutilSafeCall(cudaMemcpy(devResultAddrHost, devResultAddr, dataLen*sizeof(unsigned int), cudaMemcpyDeviceToHost));
       for (int i = 0; i < dataLen; ++i) {
-	//				printf("%i %i\n ", i, devResultAddrHost[i]);
+          printf("%i) %i\n ", i, devResultAddrHost[i]);
       }
       free(devResultAddrHost);
-      printf("%i %f\n", bytesCopy, maxCodeLen);
-      //cutilSafeCall(cudaMemcpy(result, devResult, ceilf((float)bytesCopy / 8), cudaMemcpyDeviceToHost));
 
       cudaFree(devData);
       cudaFree(devResultAddr);
