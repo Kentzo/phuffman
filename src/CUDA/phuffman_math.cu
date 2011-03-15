1f0cd05cc16a044df381b60c5943af6fbdd2590f
Ilya Kulakov <kulakov.ilya@gmail.com>
2011-03-15 20:16:10 +0600

    Use uint/uint2/uint4 instead of int/int2/int4

--- src/CUDA/phuffman_math.cu
+++ src/CUDA/phuffman_math.cu
@@ -3,82 +3,82 @@
 
 namespace phuffman {
     namespace CUDA {
-        __host__ __device__ int4 operator<<(int4 value, unsigned int positions) {
-            int x_l = value.x << (positions % bytes_to_bits(sizeof(int)));
+        __host__ __device__ uint4 operator<<(uint4 value, unsigned int positions) {
+            uint x_l = value.x << (positions % bytes_to_bits(sizeof(uint)));
             
-            int y_l = value.y << (positions % sizeof(int));
-            int y_h = value.y >> (sizeof(int) - (positions % bytes_to_bits(sizeof(int))));
+            uint y_l = value.y << (positions % sizeof(uint));
+            uint y_h = value.y >> (sizeof(uint) - (positions % bytes_to_bits(sizeof(uint))));
             
-            int z_l = value.z << (positions % bytes_to_bits(sizeof(int)));
-            int z_h = value.z >> (bytes_to_bits(sizeof(int)) - (positions % bytes_to_bits(sizeof(int))));
+            uint z_l = value.z << (positions % bytes_to_bits(sizeof(uint)));
+            uint z_h = value.z >> (bytes_to_bits(sizeof(uint)) - (positions % bytes_to_bits(sizeof(uint))));
             
-            int w_l = value.w << (positions % bytes_to_bits(sizeof(int)));
-            int w_h = value.w >> (bytes_to_bits(sizeof(int)) - (positions % bytes_to_bits(sizeof(int))));
+            uint w_l = value.w << (positions % bytes_to_bits(sizeof(uint)));
+            uint w_h = value.w >> (bytes_to_bits(sizeof(uint)) - (positions % bytes_to_bits(sizeof(uint))));
             
-            int tmp[8] = {x_l | y_h, y_l | z_h, z_l | w_h, w_l};
-            unsigned int offset = positions / bytes_to_bits(sizeof(int));
-            return make_int4(tmp[0 + offset], tmp[1 + offset], tmp[2 + offset], tmp[3 + offset]);
+            uint tmp[8] = {x_l | y_h, y_l | z_h, z_l | w_h, w_l};
+            unsigned int offset = positions / bytes_to_bits(sizeof(uint));
+            return make_uint4(tmp[0 + offset], tmp[1 + offset], tmp[2 + offset], tmp[3 + offset]);
         }
         
-        __host__ __device__ int2 operator<<(int2 value, unsigned int positions) {
-            int x_l = value.x << (positions % bytes_to_bits(sizeof(int)));
+        __host__ __device__ uint2 operator<<(uint2 value, unsigned int positions) {
+            uint x_l = value.x << (positions % bytes_to_bits(sizeof(uint)));
             
-            int y_l = value.y << (positions % bytes_to_bits(sizeof(int)));
-            int y_h = value.y >> (bytes_to_bits(sizeof(int)) - (positions % bytes_to_bits(sizeof(int))));
+            uint y_l = value.y << (positions % bytes_to_bits(sizeof(uint)));
+            uint y_h = value.y >> (bytes_to_bits(sizeof(uint)) - (positions % bytes_to_bits(sizeof(uint))));
             
-            int tmp[4] = {x_l | y_h, y_l};
-            unsigned int offset = positions / bytes_to_bits(sizeof(int));
-            return make_int2(tmp[0 + offset], tmp[1 + offset]);
+            uint tmp[4] = {x_l | y_h, y_l};
+            unsigned int offset = positions / bytes_to_bits(sizeof(uint));
+            return make_uint2(tmp[0 + offset], tmp[1 + offset]);
         }
         
-        __host__ __device__ int4 operator>>(int4 value, unsigned int positions) {
-            int x_h = value.x >> (positions % bytes_to_bits(sizeof(int)));
-            int x_l = value.x << (bytes_to_bits(sizeof(int)) - (positions % bytes_to_bits(sizeof(int))));
+        __host__ __device__ uint4 operator>>(uint4 value, unsigned int positions) {
+            uint x_h = value.x >> (positions % bytes_to_bits(sizeof(uint)));
+            uint x_l = value.x << (bytes_to_bits(sizeof(uint)) - (positions % bytes_to_bits(sizeof(uint))));
         
-            int y_h = value.y >> (positions % bytes_to_bits(sizeof(int)));
-            int y_l = value.y << (bytes_to_bits(sizeof(int)) - (positions % bytes_to_bits(sizeof(int))));
+            uint y_h = value.y >> (positions % bytes_to_bits(sizeof(uint)));
+            uint y_l = value.y << (bytes_to_bits(sizeof(uint)) - (positions % bytes_to_bits(sizeof(uint))));
         
-            int z_h = value.z >> (positions % bytes_to_bits(sizeof(int)));
-            int z_l = value.z << (bytes_to_bits(sizeof(int)) - (positions % bytes_to_bits(sizeof(int))));
+            uint z_h = value.z >> (positions % bytes_to_bits(sizeof(uint)));
+            uint z_l = value.z << (bytes_to_bits(sizeof(uint)) - (positions % bytes_to_bits(sizeof(uint))));
             
-            int w_h = value.w >> (positions % bytes_to_bits(sizeof(int)));
+            uint w_h = value.w >> (positions % bytes_to_bits(sizeof(uint)));
             
             int tmp[8] = {[4] = x_h, x_l | y_h, y_l | z_h, z_l | w_h};
-            unsigned int offset = positions / bytes_to_bits(sizeof(int));
-            return make_int4(tmp[4 - offset], tmp[5 - offset], tmp[6 - offset], tmp[7 - offset]);
+            unsigned int offset = positions / bytes_to_bits(sizeof(uint));
+            return make_uint4(tmp[4 - offset], tmp[5 - offset], tmp[6 - offset], tmp[7 - offset]);
         }
         
-        __host__ __device__ int2 operator>>(int2 value, unsigned int positions) {
-            int x_h = value.x >> (positions % bytes_to_bits(sizeof(int)));
-            int x_l = value.x << (bytes_to_bits(sizeof(int)) - (positions % bytes_to_bits(sizeof(int))));
+        __host__ __device__ uint2 operator>>(uint2 value, unsigned int positions) {
+            uint x_h = value.x >> (positions % bytes_to_bits(sizeof(uint)));
+            uint x_l = value.x << (bytes_to_bits(sizeof(uint)) - (positions % bytes_to_bits(sizeof(uint))));
             
-            int y_h = value.y >> (positions % bytes_to_bits(sizeof(int)));
+            uint y_h = value.y >> (positions % bytes_to_bits(sizeof(uint)));
             
-            int tmp[4] = {[2] = x_h, x_l | y_h};
-            unsigned int offset = positions / bytes_to_bits(sizeof(int));
-            return make_int2(tmp[2 - offset], tmp[3 - offset]);
+            uint tmp[4] = {[2] = x_h, x_l | y_h};
+            unsigned int offset = positions / bytes_to_bits(sizeof(uint));
+            return make_uint2(tmp[2 - offset], tmp[3 - offset]);
         }
 
-        __host__ __device__ void operator<<=(int4 &value, unsigned int positions) {
+        __host__ __device__ void operator<<=(uint4 &value, unsigned int positions) {
             value = value << positions;
         }
         
-        __host__ __device__ void operator<<=(int2 &value, unsigned int positions) {
+        __host__ __device__ void operator<<=(uint2 &value, unsigned int positions) {
             value = value << positions;
         }        
         
-        __host__ __device__ void operator>>=(int4 &value, unsigned int positions) {
+        __host__ __device__ void operator>>=(uint4 &value, unsigned int positions) {
             value = value >> positions;
         }
         
-        __host__ __device__ void operator>>=(int2 &value, unsigned int positions) {
+        __host__ __device__ void operator>>=(uint2 &value, unsigned int positions) {
             value = value >> positions;
         }
         
-        __host__ __device__ int4 merge_int2(int2 a, unsigned int a_effective_size, int2 b) {
-            int2 b_h = b >> a_effective_size;
-            int2 b_l = b << bytes_to_bits(sizeof(int2)) - a_effective_size;
-            return make_int4(a | b_h, b_l);
+        __host__ __device__ uint4 merge_uint2(uint2 a, unsigned int a_effective_size, uint2 b) {
+            uint2 b_h = b >> a_effective_size;
+            uint2 b_l = b << bytes_to_bits(sizeof(uint2)) - a_effective_size;
+            return make_uint4(a | b_h, b_l);
         }
     }
 }
--- src/CUDA/phuffman_math.cuh
+++ src/CUDA/phuffman_math.cuh
@@ -15,85 +15,85 @@ namespace phuffman {
         }
 
         /*!
-          @abstract Creates int4 from two int2 objects.
+          @abstract Creates uint4 from two uint2 objects.
         */
-        inline __host__ __device__ int4 make_int4(int2 a, int2 b) {
-            return make_int4(a.x, a.y, b.x, b.y);
+        inline __host__ __device__ uint4 make_uint4(uint2 a, uint2 b) {
+            return make_uint4(a.x, a.y, b.x, b.y);
         }
 
         /*!
           @abstract Shifts positions bits of a given value to the left and returns the result.
           @param value A value to be shifted to the left.
-          @param positions A number of bits to shift. MUST NOT be greater than bytes_to_bits(sizeof(int4)).
+          @param positions A number of bits to shift. MUST NOT be greater than bytes_to_bits(sizeof(uint4)).
           @result The shifted value.
         */
-        __host__ __device__ int4 operator<<(int4 value, unsigned int positions);
+        __host__ __device__ uint4 operator<<(uint4 value, unsigned int positions);
 
         /*!
           @abstract Shifts positions bits of a given value to the left and returns the result.
           @param value A value to be shifted to the left.
-          @param positions A number of bits to shift. MUST NOT be greater than bytes_to_bits(sizeof(int2)).
+          @param positions A number of bits to shift. MUST NOT be greater than bytes_to_bits(sizeof(uint2)).
           @result The shifted value.
         */
-        __host__ __device__ int2 operator<<(int2 value, unsigned int positions);
+        __host__ __device__ uint2 operator<<(uint2 value, unsigned int positions);
 
         /*!
           @abstract Shifts positions bits of a given value to the right and returns the result.
           @param value A value to be shifted to the right.
-          @param positions A number of bits to shift. MUST NOT be greater than bytes_to_bits(sizeof(int4)).
+          @param positions A number of bits to shift. MUST NOT be greater than bytes_to_bits(sizeof(uint4)).
           @result The shifted value.
         */        
-        __host__ __device__ int4 operator>>(int4 value, unsigned int positions);
+        __host__ __device__ uint4 operator>>(uint4 value, unsigned int positions);
 
         /*!
           @abstract Shifts positions bits of a given value to the right and returns the result.
           @param value A value to be shifted to the right.
-          @param positions A number of bits to shift. MUST NOT be greater than bytes_to_bits(sizeof(int2)).
+          @param positions A number of bits to shift. MUST NOT be greater than bytes_to_bits(sizeof(uint2)).
           @result The shifted value.
         */                
-        __host__ __device__ int2 operator>>(int2 value, unsigned int positions);
+        __host__ __device__ uint2 operator>>(uint2 value, unsigned int positions);
 
         /*!
           @abstract Performs left-shift in place.
         */
-        __host__ __device__ void operator<<=(int4 &value, unsigned int positions);
+        __host__ __device__ void operator<<=(uint4 &value, unsigned int positions);
         
         /*!
           @abstract Performs left-shift in place.
         */
-        __host__ __device__ void operator<<=(int2 &value, unsigned int positions);
+        __host__ __device__ void operator<<=(uint2 &value, unsigned int positions);
 
         /*!
           @abstract Performs right-shift in place.
         */        
-        __host__ __device__ void operator>>=(int4 &value, unsigned int positions);
+        __host__ __device__ void operator>>=(uint4 &value, unsigned int positions);
         
         /*!
           @abstract Performs right-shift in place.
         */
-        __host__ __device__ void operator>>=(int2 &value, unsigned int positions);
+        __host__ __device__ void operator>>=(uint2 &value, unsigned int positions);
         
         /*!
           @abstracts ORs given values.
         */
-        inline __host__ __device__ int4 operator|(int4 a, int4 b) {
-            return make_int4(a.x | b.x, a.y | b.y, a.z | b.z, a.w | b.w);
+        inline __host__ __device__ uint4 operator|(uint4 a, uint4 b) {
+            return make_uint4(a.x | b.x, a.y | b.y, a.z | b.z, a.w | b.w);
         }
 
         /*!
           @abstracts ORs given values.
         */
-        inline __host__ __device__ int2 operator|(int2 a, int2 b) {
-            return make_int2(a.x | b.x, a.y | b.y);
+        inline __host__ __device__ uint2 operator|(uint2 a, uint2 b) {
+            return make_uint2(a.x | b.x, a.y | b.y);
         }
 
         /*!
-          @abstract Merges two int2 values into one int4 overriding unnecessary bits.
+          @abstract Merges two uint2 values into one uint4 overriding unnecessary bits.
           @param a This value will be left in the result.
-          @param a_effective_size Number of leftmost bits that make sense. MUST NOT be greater than bytes_to_bits(sizeof(int2)).
+          @param a_effective_size Number of leftmost bits that make sense. MUST NOT be greater than bytes_to_bits(sizeof(uint2)).
           @param b This valie will be right in the result.
         */
-        __host__ __device__ int4 merge_int2(int2 a, unsigned int a_effective_size, int2 b);
+        __host__ __device__ uint4 merge_uint2(uint2 a, unsigned int a_effective_size, uint2 b);
     }
 }
 #endif // __CUDACC__
